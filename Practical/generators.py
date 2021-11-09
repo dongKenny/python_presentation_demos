3fe7659a49ecce6f1cda0a9af1a58b626f42b157
dongKenny <kdong4@dons.usfca.edu>
2021-11-08 19:06:15 -0800

    Generator partial

---
+++ Practical/generators.py
@@ -0,0 +1,43 @@
+import time
+import memory_profiler as mem_profile
+
+
+def get_square_nums_list(n: int = 100_000_000):
+    return [i**2 for i in range(n)]
+
+
+def get_square_nums_comprehension(n: int = 100_000_000):
+    return (i**2 for i in range(n))
+
+
+def get_square_nums(n: int = 100_000_000):
+    for i in range(n):
+        yield i**2
+
+
+def main():
+    # print(f"Memory before list: {str(mem_profile.memory_usage())}MB")
+    # start_time = time.time_ns()
+    # squares_list = get_square_nums_list()
+    # end = time.time_ns()
+    # print(f"List Squares Time: {end-start_time}")
+    # print(f"Memory after list: {str(mem_profile.memory_usage())}MB")
+    # for i, square in enumerate(squares_list):
+    #     print(square)
+    #     if i > 50:
+    #         break
+
+    print(f"Memory before generator: {str(mem_profile.memory_usage())}MB")
+    start_time = time.time_ns()
+    squares_generator = get_square_nums_comprehension()
+    end = time.time_ns()
+    print(f"Generator Squares Time: {end - start_time}")
+    print(f"Memory after generator: {str(mem_profile.memory_usage())}MB")
+    for i, square in enumerate(squares_generator):
+        print(square)
+        if i > 50:
+            break
+
+
+if __name__ == "__main__":
+    main()
